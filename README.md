be7184cf04a078b4ce213a494a7e92aa54f03cc3
Qualcommes <qualcommes122@gmail.com>
2026-04-21 02:52:04 +0700

    fourth step

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Qualcommes/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Qualcommes/python-project-50/actions)
+### Example of using a difference generator
+[![asciicast](https://asciinema.org/a/yuA3UN3QMLw4Eirg.svg)](https://asciinema.org/a/yuA3UN3QMLw4Eirg)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,36 @@ import argparse
 import json
 
 
+def generate_diff(file_path1: str, file_path2: str) -> str:
+    with open(file_path1, encoding='utf-8') as file:
+        file1 = json.load(file)
+    with open(file_path2, encoding='utf-8') as file:
+        file2 = json.load(file)
+    set1 = set(file1)
+    set2 = set(file2)
+    difference = set1 - set2
+    intersection = set1 & set2
+    plus = set2 - set1
+    all = sorted(set1 | set2)
+    result = '{\n'
+    for key in all:
+        if key in difference:
+            result += '  - ' + key + ': ' + str(file1[key]).lower() + '\n'
+            continue
+        elif key in intersection:
+            if file1[key] == file2[key]:
+                result += '    ' + key + ': ' + str(file1[key]).lower() + '\n'
+            else:
+                result += '  - ' + key + ': ' + str(file1[key]).lower() + \
+                '\n  + ' + key + ': ' + str(file2[key]).lower() + '\n'
+            continue
+        elif key in plus:
+            result += '  + ' + key + ': ' + str(file2[key]).lower() + '\n'
+            continue
+    result += '}'
+    return result
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
@@ -16,10 +46,8 @@ def main():
     
     # обработка
     print(f"Comparing {args.first_file} and {args.second_file}...")
-    with open(args.first_file, encoding='utf-8') as file:
-        file1 = json.load(file)
-    with open(args.second_file, encoding='utf-8') as file:
-        file2 = json.load(file)
+    result = generate_diff(args.first_file, args.second_file)
+    print(result)
         
 
 if __name__ == "__main__":
